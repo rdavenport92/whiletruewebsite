f888ee2295c9de8799208b005d2dc14409b46109
Ricky Davenport <ricky.davenport@wwt.com>
2025-06-30 15:58:21 -0500

    remove about me

--- src/components/NavItems.vue
+++ src/components/NavItems.vue
@@ -8,7 +8,7 @@
       >Merch</a
     >
     <router-link to="/contact">Contact</router-link>
-    <router-link to="/about">About</router-link>
+    <!-- <router-link to="/about">About</router-link> -->
   </nav>
 </template>
 <style scoped>
